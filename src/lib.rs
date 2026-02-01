c36fdb2a061232b8c849736407e6aeb8c242c6d0
bendn <bend.n@outlook.com>
2026-02-02 02:18:28 +0700

    use copy_to_slice instead of store_select_unchecked

--- src/lib.rs
+++ src/lib.rs
@@ -548,7 +548,8 @@ fn into(
                     if cfg!(miri) {
                         to_b.copy_from_slice(&result.to_array());
                     } else {
-                        result.store_select(to_b, Mask::from_bitmask(!0));
+                        unsafe { core::hint::assert_unchecked(to_b.len()==$n*3) };
+                        result.copy_to_slice(to_b);
                     }
                     wx_ += $n;
                 }
@@ -556,10 +557,10 @@ fn into(
         }
         read!(while 10);
         read!(if 4);
-		let mut n = with.width() - wx_;
-		if n + x_ + wx_ > i.width() {
-    		n = i.width() - x_ - wx_;   
-		}
+        let mut n = with.width() - wx_;
+        if n + x_ + wx_ > i.width() {
+            n = i.width() - x_ - wx_;
+        }
         unsafe { core::hint::assert_unchecked(n < 10) };
         for k in 0..n {
             let x = k + wx_;
